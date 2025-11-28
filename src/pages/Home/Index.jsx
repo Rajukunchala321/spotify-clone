96511340e2529b1f6e3647853c486ac2b46970a0
raju <rajukunchla321@gmail>
2025-11-28 17:56:55 +0530

    basic code build

---
+++ src/pages/Home/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Index.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to Spotify Clone</h1>
+      <p>Explore your favorite music</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/LoginPage/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Index.css"
+const Index = () => {
+  return (
+    <section className='loginpage-section'>
+        <div>
+            <div>
+                Login
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Index;
