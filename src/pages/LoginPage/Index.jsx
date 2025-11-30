5b35e06e2fdb657aebc7a90c85fba6a0d3b68a57
raju <rajukunchala709@gmail>
2025-11-30 17:45:51 +0530

    commit

--- src/pages/LoginPage/Index.jsx
+++ src/pages/LoginPage/Index.jsx
@@ -3,9 +3,17 @@ import "./Index.css"
 const Index = () => {
   return (
     <section className='loginpage-section'>
-        <div>
-            <div>
-                Login
+        <div className="loginpage-main-container">
+            <div className='form-container'>
+              <div>
+                <img  src='/src/assests/login-bg.png' alt='logo' />
+                <h2>Spotify Remix</h2>
+                <form>
+                  
+                </form>
+
+              </div>
+              
             </div>
         </div>
     </section>
